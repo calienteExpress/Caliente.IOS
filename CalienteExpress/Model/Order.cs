a4e7b62a32c41dcb6854f7a861bfca87b46ff804
John R. Kosinski <john.kosinski@gmail.com>
2016-02-06 04:44:22 +0700

    First commit

---
+++ CalienteExpress/Model/Order.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalienteExpress.Model
+{
+    public class Order
+    {
+    }
+}
---
+++ CalienteExpress/Model/OrderItem.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalienteExpress.Model
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+    }
+}
